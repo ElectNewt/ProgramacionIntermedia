bff54a5a871d6f5988fae2536c464a3ba7e9c447
ivan abad <ivan.abad92@gmail.com>
2019-10-08 18:00:58 +0100

    add Sobrecarga de métodos en C#

---
+++ Sobrecarga/sobrecargaDeMetodos/sobrecargaDeMetodos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace sobrecargaDeMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sobrecarga sobrecarga = new Sobrecarga();
+            sobrecarga.Suma(10, "10");
+           
+            Console.WriteLine(sobrecarga.Suma(10, 15,20));
+        }
+    }
+}
---
+++ Sobrecarga/sobrecargaDeMetodos/sobrecargaDeMetodos/sobrecarga.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sobrecargaDeMetodos
+{
+    public class Sobrecarga
+    {
+        public int Suma(int item1, int item2)
+        {
+            return item1 + item2;
+        }
+
+        public int Suma(int item1, int item2, int item3)
+        {
+            return item1 + item2 + item3;
+        }
+
+        public string Suma(int item1, string item2)
+        {
+            return $" suma entre {item1} y {item2}";
+        }
+
+        public string Suma(string item1, int item2)
+        {
+            return $" suma entre {item1} y {item2}";
+        }
+       
+
+        
+    }
+}
