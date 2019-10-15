5f80f35f9bddb07710bc7bf15b1d9768b3237fd3
ivan abad <ivan.abad92@gmail.com>
2019-10-15 17:46:11 +0100

    add clase abstracta

---
+++ Abstract/ClaseAbstracta/ClaseAbstracta/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClaseAbstracta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Acutangulo acutangulo = new Acutangulo();
+            Escaleno escaleno = new Escaleno();
+
+            acutangulo.Area();
+            escaleno.Area();
+
+            acutangulo.CalcularAreaEnBaseAngulos(2, 3, 4);
+
+            escaleno.CalcularAreaTrianguloconHipotenusa(2, 5);
+            acutangulo.CalcularAreaTrianguloconHipotenusa(2, 5);
+
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+
+}
---
+++ Abstract/ClaseAbstracta/ClaseAbstracta/Triangulo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClaseAbstracta
+{
+    public abstract class TrianguloBase
+    {
+        public abstract decimal Perimetro();
+        public abstract decimal Area();
+
+        public double CalcularAreaTrianguloconHipotenusa(int lado, int hipotenusa)
+        {
+            double ladob = Math.Sqrt(Math.Pow(hipotenusa, 2) - Math.Pow(lado, 2));
+            return lado * ladob / 2;
+        }
+    }
+
+    public class Escaleno : TrianguloBase
+    {
+        public override decimal Area()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal Perimetro()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+    public class Acutangulo : TrianguloBase
+    {
+        public override decimal Area()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal Perimetro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CalcularAreaEnBaseAngulos(int angulo1, int angulo2, int angulo3)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
