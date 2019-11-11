53392c70047cb18b21907e14b5b6cea5e683f575
ivan abad <ivan.abad92@gmail.com>
2019-11-11 12:54:09 +0000

    add exension methods

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!".PrimeraLetraMayuscula());
+            Console.ReadKey();
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string PrimeraLetraMayuscula(this string frase)
+        {
+            char primeraLetra = char.ToUpper(frase[0]);
+            string restoFrase = frase.Substring(1);
+            return primeraLetra + restoFrase;
+        }
+
+    }
+
+}
