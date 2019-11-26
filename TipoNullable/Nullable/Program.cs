97ece2f9ebd52843a64d2f5e5ce00d4351932cd9
ivan abad <ivan.abad92@gmail.com>
2019-11-26 16:16:48 +0000

    add nullable

---
+++ TipoNullable/Nullable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? enteroNulo = null;
+            enteroNulo = 12;
+
+            int enteroNormal = 34;
+            
+            if (enteroNulo.HasValue)
+            {
+                enteroNormal = enteroNulo.Value;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
