608a3e49223cd53af60f1084a73801e876752e19
ivan abad <ivan.abad92@gmail.com>
2019-12-03 16:53:09 +0000

    add excepciones propias

---
+++ ExcepcionesPropias/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExcepcionesPropias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
