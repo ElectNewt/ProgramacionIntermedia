3b372a3734f03a3625ca182a1ae659016df8555d
ivan abad <ivan.abad92@gmail.com>
2019-12-17 16:19:58 +0000

    add tipo dynamic

---
+++ Dynamictype/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Dynamictype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic variableDinamica = 1;
+            Console.WriteLine(variableDinamica);
+            variableDinamica = "Hola";
+
+            Console.WriteLine(variableDinamica.GetType().ToString());
+
+            variableDinamica = new EjemploTest() { Valor = "Test valor" };
+            Console.WriteLine(variableDinamica.GetType().ToString());
+            TestDynamic(new EjemploTest() { Valor = "Test valor" });
+            TestDynamic("Hola");
+
+        }
+
+        static void TestDynamic(dynamic valor)
+        {
+            Console.WriteLine(valor.Valor);
+        }
+
+        public class EjemploTest
+        {
+            public string Valor { get; set; }
+
+        }
+    }
+}
